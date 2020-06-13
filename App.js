3855eae46fe75a2dde2004bb6bca57737339fc14
rui-watanabe <wataru610806@gmail.com>
2020-06-13 22:38:52 +0900

    add .prettierrc

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
+      <Text> Hello React Native </Text>{' '}
     </View>
   );
 }
