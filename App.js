dd7eca842bdd0ea35433984c80f5ffd57983adb2
rui-watanabe <wataru610806@gmail.com>
2020-06-17 21:56:27 +0900

    indicated title author image from api

--- App.js
+++ App.js
@@ -3,21 +3,30 @@ import { StyleSheet, View , FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem';
 import dummyArticles from './dummies/articles.json';
 import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
-    alert(Constants.manifest.extra.newsApiKey)
-    const timer = setTimeout(() => {
-      setArticles(dummyArticles)
-    }, 2000);
-    return () => clearTimeout(timer);
+   fetchArticles();    
   }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+      console.log(response.data.articles)
+    } catch(error) {
+      console.error(error);
+    }
+  }
 
   return (
     <SafeAreaView style={ styles.container }>
       <FlatList
-        data={articles}
+        data={ articles}
         renderItem={({ item }) => (
           <ListItem
           author={ item.author }
