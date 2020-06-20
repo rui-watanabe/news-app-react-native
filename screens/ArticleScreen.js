4f94bb319c38d1a8ddc9df40b188af69fe2c8ff6
rui-watanabe <wataru610806@gmail.com>
2020-06-20 12:37:22 +0900

    add indicate bookmark icon

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addClip, deleteClip } from '../store/actions/user'; 
+import ClipButton from '../components/ClipButton';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,15 +15,24 @@ const styles = StyleSheet.create({
 export default ArticleScreen = ({ route }) => {
   const { article } = route.params;
   const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
+  const { clips } = user;
+
+  const isClipped = () => {
+    return clips.some(clip => clip.url === article.url)
+  }
+
+  const toggleClip = () => {
+    if(isClipped()){
+      dispatch(deleteClip({ clip: article }));
+    } else {
+      dispatch(addClip({ clip: article }));
+    }
+  }
 
   return(
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity onPress={() => {dispatch(addClip({ clip: article }))}}>
-        <Text style={{margin: 10, fontSize: 30}}>ADD_CLIP</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => {dispatch(deleteClip({ clip: article }))}}>
-        <Text style={{margin: 10, fontSize: 30}}>DELETE_CLIP</Text>
-      </TouchableOpacity>
+      <ClipButton onPress={ toggleClip } enabled={ isClipped() } /> 
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   )
