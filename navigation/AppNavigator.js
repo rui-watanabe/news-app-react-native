e563466816bf2de31bfbdadaea6ebab81c8123e3
rui-watanabe <wataru610806@gmail.com>
2020-06-20 11:38:00 +0900

    add tab icon

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons'
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen'
 import ClipScreen from '../screens/ClipScreen';
@@ -24,15 +25,27 @@ const ClipStack = () => {
       <Stack.Screen name="Clip" component={ ClipScreen } />
     </Stack.Navigator>
   )
-}
+};
+
+const ScreenOption = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    let iconName;
+    if (route.name === 'Home') {
+      iconName = 'home';
+    } else if (route.name === 'Clip') {
+      iconName = 'bookmark';
+    }
+    return <FontAwesome name={iconName} size={size} color={color} />;
+  } 
+})
 
 export default AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={ ScreenOption }>
         <Tab.Screen name="Home" component={ HomeStack } />
         <Tab.Screen name="Clip" component={ ClipStack } />
       </Tab.Navigator>
     </NavigationContainer> 
   );
-}
+};
