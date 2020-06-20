e486258a4fb2b23e6319648971ca77c3a5d7f04f
rui-watanabe <wataru610806@gmail.com>
2020-06-20 11:12:35 +0900

    add bottom tab

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen'
+import ClipScreen from '../screens/ClipScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={ HomeScreen } />
+      <Stack.Screen name="Article" component={ ArticleScreen } />
+    </Stack.Navigator>
+  );
+};
+
+const ClipStack = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Clip" component={ ClipScreen } />
+    </Stack.Navigator>
+  )
+}
 
 export default AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={ HomeScreen } />
-        <Stack.Screen name="Article" component={ ArticleScreen } />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={ HomeStack } />
+        <Tab.Screen name="Clip" component={ ClipStack } />
+      </Tab.Navigator>
     </NavigationContainer> 
   );
 }
